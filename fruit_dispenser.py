44a62c6972758bceb100d6232a2f1c75fc9ccea6
Nathalie Vidal <88549333+Moss2021@users.noreply.github.com>
2022-10-03 13:32:19 -0400

    Update fruit_dispenser.py
    Adding while loop

--- fruit_dispenser.py
+++ fruit_dispenser.py
@@ -1,5 +1,7 @@
+import inquirer
+from datetime import date 
+
 #Fruits in Ascii:
-#Testing of comment
 apple_ascii = '''
            .:'
          __ :'__
@@ -41,56 +43,47 @@ pear_ascii = '''
               '-._  ~  _.-'
                   `''`
 '''
-
-#Declare functions:
-
-def choose_fruit():
-    print("Please select the fruit you want from the list provided! ")
-    print("Which fruit do you want? ")
-
+#Date
+print("Welcome to the FRUIT Dispenser!\n")
+today = date.today()
+print("Today's date:", today)
+print(" \n")
 #Ask user for name and fruit choice
-name = input("What is your name? ")
-print("Hello " + name + ", would you like a fruit? ")
-userReply = input("Enter yes or no: ")
+name = input("What is your name? \n")
+print("Hello " + name + ", would you like a fruit? \n")
+userReply = input("Enter yes or no: \n").lower()
+
+
 #YESSSSSSSSSSSSSSSS
-if userReply == "yes":
-    print("Please select the fruit you want from below: ")
-    fruitDict = {}
-    option = []
-    fruitList = ["apple", "banana", "pear"]
-    fruitascii = [apple_ascii, banana_ascii, pear_ascii]
-    for index in range(len(fruitList)): #len function returns the number of items in an object
-        opt = str(index + 1)
-        option.append(opt)
-        fruitDict[opt] = fruitascii[index]
-        print(opt + ". " + fruitList[index])
-       
- ############################   
-    answer = input("Chosen fruit> ").lower()
-    #if answer != fruit_indx:
-        #choose_fruit()
-        #answer = input("Chosen fruit> ").lower()  
+
+while userReply == "yes":    
+                    questions = [
+                        inquirer.List('fruit',
+                                      message="Please select a fruit from below: ",
+                                      choices=['apple', 'banana', 'pear']
+                                      ),
+                    ]
+                    answers = inquirer.prompt(questions)
+                    print("Here you go! Enjoy")
+                    if (answers['fruit'] == "apple"):
+                        print(apple_ascii)
+                    if (answers['fruit'] == "banana"):
+                        print(banana_ascii)
+                    if (answers['fruit'] == "pear"):
+                        print(pear_ascii)
+                    
+                    print(answers)
+                    print(" \n")
+                    print(name + " would you like another fruit?\n")
+                    userReply = input("Enter yes or no: \n").lower()
     
-    fruit_indx = fruitList.index(answer) #fruit list
-    fruit_indx = str(fruit_indx + 1)
-    
-    print(option)
-    for op in option:
-        if fruit_indx == op:
-            print("Here you go! ")
-            print(fruitDict[op])
-            
-        #else:
-            #fruit_indx != fruitList.index(answer)
-            #choose_fruit()
-        #answer = input("Chosen fruit> ").lower()   
-           
-#if user says "No" to fruit:        
+
+#Answer NO:       
 else:
-    print("No worries! " + name + ", please come back when you are hungry! ")
-    
-print("Thank you for playing")
+    print(" \n")
+    print("No worries! Please come back when you are hungry! \n")
+    print("Thank you for playing " + name + " !!!")
 
 
-        
+
 
