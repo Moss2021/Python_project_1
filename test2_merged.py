1907ce6fea3c0835161ec6c174a8145d22a0ae1e
Nola Larson <101831240+nolarson@users.noreply.github.com>
2022-10-04 13:42:55 -0500

    Update test2_merged.py

--- test2_merged.py
+++ test2_merged.py
@@ -82,7 +82,8 @@ print(" \n")
 print("Ok! Please choose what you want to do with the fruit! (type 1 to 4)\n(type 5 for PAYMENT or type 6 to EXIT )")
 print(" \n")   
 
-c=0
+Initialize values of variables:
+menu_item=0
 sale = 0
 quanitiy = 0
 total_received = 0
@@ -90,11 +91,13 @@ change_received = 0
 
 print("1.Shaved Ice - $5", "\n2.Smoothie - $6", "\n3.Ice Cream - $4", "\n4.Aguas de fruta(fruit drinks) - $2","\n5.Payment", "\n6.Exit \n")
 
+#this loop continues while the condition is true
 while (1):
 
-            c = int(input("Please type the number of your choice: "))
+            #Menu's choice & quantity determine the sale amount
+            menu_item = int(input("Please type the number of your choice: "))
 
-            if (c == 1):
+            if (menu_item == 1):
 
                 quantity = int(input("Enter the quantity: "))
                 sale = sale + 5 * quantity
@@ -110,15 +113,19 @@ while (1):
             elif (c == 4):
                 quantity = int(input("Enter the quantity: "))
                 sale = sale + 2 * quantity
-
-            elif (c == 5):
+                      
+            #when customer is finished ordering, the toal sale is calculated
+            elif (menu_item == 5):
                 print("Total Sale:", sale)
                 if (sale > 0):
+                    #Customer is prompted to input amount they will use to pay sale
                     total_received = int(input("Total payment received: "))
+                    #Calculate change
                     print("Your change: ", total_received - sale)
                     print("*****Thank You Come Again!!!*****")
                     quit()
-            elif (c == 6):
+            #customer wants to end order
+            elif (menu_item == 6):
                     quit()
             else:
                     print("Please enter a valid option (1-5): ")
