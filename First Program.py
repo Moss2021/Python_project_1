e6f0abe94a19b6fa69a9ca2957681b6d3a1cf30d
Nola Larson <101831240+nolarson@users.noreply.github.com>
2022-10-04 21:30:13 -0500

    Update First Program.py

--- First Program.py
+++ First Program.py
@@ -1,44 +1,52 @@
-c=0
+#Initialize values of variables:
+menu_item = 0
 sale = 0
-quanitiy = 0
+quantity = 0
 total_received = 0
 change_received = 0
 
-print("Fruta Raspada")
-        
+print("1.Shaved Ice - $5", "\n2.Smoothie - $6", "\n3.Ice Cream - $4", "\n4.Aguas de fruta(fruit drinks) - $2","\n5.Payment", "\n6.Exit \n")
 
-print("1.Shaved Ice - 5", "\n2.Smoothie - 6", "\n3.ice cream - 4", "\n4.Aguas de fruta(fruit drinks) - 2","\n5.End of order", "\n6.Exit")
-
+#this loop continues while the condition is true
 while (1):
-
-            c = int(input("Please enter your Menu Item Number or 5 to end order: "))
-
-            if (c == 1):
+           
+            #Menu's choice & quantity determine the sale amount
+            menu_item = int(input("Please type the number of your choice: "))
+                           
+            if (menu_item == 1):
 
                 quantity = int(input("Enter the quantity: "))
                 sale = sale + 5 * quantity
 
-            elif (c == 2):
+            elif (menu_item == 2):
                 quantity = int(input("Enter the quantity: ")) 
                 sale = sale + 6 * quantity
 
-            elif (c == 3):
+            elif (menu_item == 3):
                 quantity = int(input("Enter the quantity: "))
-                sale = sale + 3 * quantity
+                sale = sale + 4 * quantity
 
-            elif (c == 4):
+            elif (menu_item == 4):
                 quantity = int(input("Enter the quantity: "))
                 sale = sale + 2 * quantity
-
-            elif (c == 5):
+                      
+            #when customer is finished ordering, the total sale is calculated
+            elif (menu_item == 5):
                 print("Total Sale:", sale)
                 if (sale > 0):
+                    #Customer is prompted to input amount they will use to pay sale
                     total_received = int(input("Total payment received: "))
-                    print("Change: ", total_received - sale)
+                    #Calculate change
+                    print("Your change: ", total_received - sale)
                     print("*****Thank You Come Again!!!*****")
                     quit()
-            elif (c == 6):
+                      
+                #when customer wants to end order
+                elif (menu_item == 6):
                     quit()
-            else:
+           
+                #if customer selects number outside of range, he is prompted to type valid option
+                else:
                     print("Please enter a valid option (1-5): ")
+
     
